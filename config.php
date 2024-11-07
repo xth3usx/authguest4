ba817a59c1593bf7ab428ab7befac9c4092fc9ca
xth3usx <matheus.hso89@gmail.com>
2024-11-07 16:06:00 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define('CLIENT_ID', '-');
+define('CLIENT_SECRET', '-');
+define('REDIRECT_URI', '-');
+define('AUTH_URL', 'https://sso.staging.acesso.gov.br/authorize');
+define('TOKEN_URL', 'https://sso.staging.acesso.gov.br/token');
+define('USERINFO_URL', 'https://sso.staging.acesso.gov.br/userinfo');
+define('JWKS_URL', 'https://sso.staging.acesso.gov.br/jwk');
+define('SCOPE', 'openid email profile govbr_confiabilidades');
+?>
